04a9bb65c328431a02fe1353ef672cc4b2f9417c
Opop29 <joshuaquidit123456@gmail.com>
2025-05-02 10:26:37 +0800

    set location app

--- src/App.tsx
+++ src/App.tsx
@@ -39,11 +39,11 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/home">
+        <Route exact path="/attendance_tracker/home">
           <Home />
         </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
+        <Route exact path="attendance_tracker/">
+          <Redirect to="attendance_tracker/home" />
         </Route>
       </IonRouterOutlet>
     </IonReactRouter>
